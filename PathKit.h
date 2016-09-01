dcb162fa3d094247c1f058a7ef7e14d1ad0c56d8
Jamie White <jamie@skim.it>
2016-09-01 11:35:41 +0100

    Adding carthage support

---
+++ PathKit.h
@@ -0,0 +1,19 @@
+//
+//  PathKit.h
+//  PathKit
+//
+//  Created by James White on 01/09/2016.
+//  Copyright © 2016 Kyle Fuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PathKit.
+FOUNDATION_EXPORT double PathKitVersionNumber;
+
+//! Project version string for PathKit.
+FOUNDATION_EXPORT const unsigned char PathKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PathKit/PublicHeader.h>
+
+
